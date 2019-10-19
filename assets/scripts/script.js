7f566cd156c4480ce667a495802961ae04da428c
Raouf <abdelrauof97@gmail.com>
2019-10-20 01:43:13 +0200

    added nav-original to mobile

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  if ($(window).scrollTop() > 1) {
-    $('.navbar-outer.nav-animate').addClass('navbar-original');
-  }
-  if ($(window).scrollTop() < 2) {
-    $('.navbar-outer.nav-animate').removeClass('navbar-original');
-  }
-  $(window).scroll(function () {
+  if (window.innerWidth > 700) {
     if ($(window).scrollTop() > 1) {
       $('.navbar-outer.nav-animate').addClass('navbar-original');
     }
     if ($(window).scrollTop() < 2) {
       $('.navbar-outer.nav-animate').removeClass('navbar-original');
     }
-  });
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 1) {
+        $('.navbar-outer.nav-animate').addClass('navbar-original');
+      }
+      if ($(window).scrollTop() < 2) {
+        $('.navbar-outer.nav-animate').removeClass('navbar-original');
+      }
+    });
+  } else {
+    $('.navbar-outer.nav-animate').addClass('navbar-original');
+  }
+
 });
