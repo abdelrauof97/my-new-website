735a6918910b85ab62fa4a05ba2c54c54327e766
Raouf <abdelrauof97@gmail.com>
2019-11-12 01:08:55 +0200

    added animation effect

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   if (window.innerWidth > 700) {
-    if ($(window).scrollTop() > 1) {
-      $('.navbar-outer.nav-animate').addClass('navbar-original');
+    if ($(window).scrollTop() > 10) {
+      $('.navbar-outer').addClass('navbar-animated');
     }
-    if ($(window).scrollTop() < 2) {
-      $('.navbar-outer.nav-animate').removeClass('navbar-original');
+    if ($(window).scrollTop() < 11) {
+      $('.navbar-outer').removeClass('navbar-animated');
     }
     $(window).scroll(function () {
-      if ($(window).scrollTop() > 1) {
-        $('.navbar-outer.nav-animate').addClass('navbar-original');
+      if ($(window).scrollTop() > 10) {
+        $('.navbar-outer').addClass('navbar-animated');
       }
-      if ($(window).scrollTop() < 2) {
-        $('.navbar-outer.nav-animate').removeClass('navbar-original');
+      if ($(window).scrollTop() < 11) {
+        $('.navbar-outer').removeClass('navbar-animated');
       }
     });
   } else {
-    $('.navbar-outer.nav-animate').addClass('navbar-original');
+    $('.navbar-outer').addClass('navbar-animated');
   }
 
 });
