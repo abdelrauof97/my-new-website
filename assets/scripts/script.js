42ec89272e5f1a52cfd0029d2a5663663c992b7b
Raouf <abdelrauof97@gmail.com>
2019-11-12 01:27:12 +0200

    execte script directly

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  if (window.innerWidth > 700) {
+if (window.innerWidth > 700) {
+  if ($(window).scrollTop() > 10) {
+    $('.navbar-outer').addClass('navbar-animated');
+  }
+  if ($(window).scrollTop() < 11) {
+    $('.navbar-outer').removeClass('navbar-animated');
+  }
+  $(window).scroll(function () {
     if ($(window).scrollTop() > 10) {
       $('.navbar-outer').addClass('navbar-animated');
     }
     if ($(window).scrollTop() < 11) {
       $('.navbar-outer').removeClass('navbar-animated');
     }
-    $(window).scroll(function () {
-      if ($(window).scrollTop() > 10) {
-        $('.navbar-outer').addClass('navbar-animated');
-      }
-      if ($(window).scrollTop() < 11) {
-        $('.navbar-outer').removeClass('navbar-animated');
-      }
-    });
-  } else {
-    $('.navbar-outer').addClass('navbar-animated');
-  }
-
-});
+  });
+} else {
+  $('.navbar-outer').addClass('navbar-animated');
+}
